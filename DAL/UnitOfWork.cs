ee7c785662bda58a1f0164aa2488823913e1c707
yMahmoudi <Y.mahmoudi.y@gmail.com>
2019-08-23 13:29:33 +0430

    add unit of work

---
+++ DAL/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DAL
+{
+    class UnitOfWork : IDisposable
+    {
+        private DatabaseContext _context;
+
+        protected virtual Models.DatabaseContext DatabaseContext => _context ?? (_context = new Models.DatabaseContext());
+
+        public void Save()
+        {
+            DatabaseContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
