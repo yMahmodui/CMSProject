dca5104b00249aeb2788f19f1813522ab1c19452
yMahmoudi <Y.mahmoudi.y@gmail.com>
2019-08-23 15:36:33 +0430

    add hasing method add method to convert time to long

---
+++ Dtx/Security/Hashing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtx.Security
+{
+    public class Hashing
+    {
+        public static string GetSha1(string value)
+        {
+            if (value == null)
+                return string.Empty;
+
+            value = value.Trim();
+            if (value == string.Empty)
+                return string.Empty;
+
+            try
+            {
+                var bytInputs = Encoding.ASCII.GetBytes(value);
+                var oHash = System.Security.Cryptography.SHA1.Create();
+                var bytHashes = oHash.ComputeHash(bytInputs);
+
+                var oStringBuilder = new StringBuilder();
+
+                foreach (var t in bytHashes)
+                    oStringBuilder.Append(t.ToString("X2"));
+
+                return (oStringBuilder.ToString());
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ Dtx/Time/JsTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtx.Time
+{
+    public static class JsTime
+    {
+        public static long ToMilliseconds(this DateTime dt)
+        {
+            return (dt.ToUniversalTime().Ticks - (new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).Ticks) / 10000;
+        }
+    }
+}
