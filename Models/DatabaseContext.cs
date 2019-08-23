248a499d24ff06286474d8a1864e5ea891ee99e3
yMahmoudi <Y.mahmoudi.y@gmail.com>
2019-08-23 13:23:05 +0430

    add database context

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("Data Source=.;Initial Catalog=MvcDB;Integrated Security=true;")
+        {
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
+    }
+}
